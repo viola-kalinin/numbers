954d9e5ccf0223d4cf266451272334ffbb2ca479
Viola Kalinin <viola.kalinin@gmail.com>
2020-11-16 11:05:55 -0800

    adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# numbers.sh
+# Viola Kalinin
+echo "Enter a positive number: "
+read NUM
+N=0
+while [ $N -lt $NUM ]
+	do 
+	N=$((N+1))
+	if [ $((N%2)) -eq 0 ]
+		then
+			echo $N "Even"
+	fi
+	if [ $((N%2)) -ne 0 ]
+                then 
+			echo $N "Odd"
+        fi
+	done
